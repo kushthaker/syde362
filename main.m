f366da50ea390951408c581100ab3970f9833e15
Tristan Kuehn <tristankuehn@gmail.com>
2018-03-19 22:50:12 -0400

    Add new changes

--- main.m
+++ main.m
@@ -1,11 +1,12 @@
 % Objective Function
 % FORM: objective = @(x) rho*((8*(pi*Rcb_l^2*Lcb_l)) + (12*(Lst_l*Tst_l*Hst_l)) + (2*(Lsp_l*Tsp_l*Hsp_l)) + (4*Tl_l*Ll_l*Hl_l) + (6*pi*Rcb_k^2*Lcb_k) + (8*(Lst_k*Tst_k*Hst_k)) + (2*(Lsp_k*Tsp_k*Hsp_k)) + (4*Tl_k*Ll_k*Hl_k));
 
-objective = @(x) x(1)*((8*(pi*x(2)^2*x(3))) + (12*(x(4)*x(5)*x(6))) + (2*(x(7)*x(8)*x(9))) + (4*x(10)*x(11)*x(12)) + (6*pi*x(13)^2*x(14)) + (8*(x(15)*x(16*x(17))) + (2*(x(18)*x(19)*x(20))) + (4*x(21)*x(22)*x(23))));
+rho = 350;
+objective = @(x) x(1)*((8*(pi*x(2)^2*x(3))) + (12*(x(4)*x(5)*x(6))) + (2*(x(7)*x(8)*x(9))) + (4*x(10)*x(11)*x(12)) + (6*pi*x(13)^2*x(14)) + (8*(x(15)*x(16)*x(17))) + (2*(x(18)*x(19)*x(20))) + (4*x(21)*x(22)*x(23)));
 
 % Initial Guess
 
-x0 = [20, 2, 5, 0.5, 0.25, 1.5, 3, 0.3, 0.15];
+x0 = [350, 0.0254 / 4, 0.3, 0.404, 0.01, 0.075, 0.404, 0.01, 0.05, 0.3, 0.01, 0.05, 0.0254 / 4, 0.3, 0.404, 0.01, 0.075, 0.404, 0.01, 0.05, 0.3, 0.01, 0.05];
 
 disp(['Initial Guess: ' num2str(objective(x0))]);
 
@@ -18,16 +19,16 @@ beq = [];
 
 % Lower and Upper Bounds
 
-lb = [];
+lb = zeros([1 23]);
 ub = [];
 
 % Non-linear constraints
 
-nonlincon = @nlcon; % Both keyboard and laptop constraints need to be in a single function
+nonlincon = @nlcon_keyboard; % Both keyboard and laptop constraints need to be in a single function
 
 [x, fval, ef, output]  = fmincon(objective, x0, A, b, Aeq, beq, lb, ub, nonlincon);
 
-[c, ceq] = nlcon(x);
+% [c, ceq] = nlcon(x);
 
 
 % keySet = {'rho', 'Rcb_l', 'Lcb_l', 'Lst_l', 'Tst_l', 'Hst_l', 'Lsp_l', 'Tsp_l', 'Hsp_l', 'Tl_l', 'Ll_l', 'Hl_l', 'Rcb_k', 'Lcb_k', 'Lst_k', 'Tst_k', 'Hst_k', 'Lsp_k', 'Tsp_k', 'Hsp_k', 'Tl_k', 'Ll_k', 'Hl_k'};
--- nlcon_keyboard.m
+++ nlcon_keyboard.m
@@ -1,4 +1,4 @@
-function [c, ceq] = nlcon_keyboard_strength(x)
+function [c, ceq] = nlcon_keyboard(x)
 
 	% keySet = {'rho', 'Rcb_l', 'Lcb_l', 'Lst_l', 'Tst_l', 'Hst_l', 'Lsp_l', 'Tsp_l', 'Hsp_l', 'Tl_l', 'Ll_l', 'Hl_l', 'Rcb_k', 'Lcb_k', 'Lst_k', 'Tst_k', 'Hst_k', 'Lsp_k', 'Tsp_k', 'Hsp_k', 'Tl_k', 'Ll_k', 'Hl_k'};
 
@@ -25,20 +25,23 @@ function [c, ceq] = nlcon_keyboard_strength(x)
 	% Tl_k = x(21);
 	% Ll_k = x(22);
 	% Hl_k = x(23);
-
+    
+    % No nonlinear equalitird
+    ceq = [];
 
 	% Material Properties
-
-	rho = x(1);
-	Est = 0;
-	Ecb = 0;
-
-	% Bearing and Pin Dimensions
-
-	r_b = 0;
-	sigma_b = 0;
-	r_l = 0;
-	sigma_l = 0;
+    rho = 350; % (x(1)) 350 kg / m^3
+	Est = 8500 * 1000; % kPa
+	Ecb = 8500 * 1000; % kPa
+	
+    % Bearing and Pin Dimensions
+    r_b = .0254 / 2 / 2; % half inch diameter
+	sigma_b = 6200; % kPa
+	r_l = .0254 / 2 / 2; % half inch diameter
+	sigma_l = 6200; % kPa
+    
+    % Factor of safety
+    K = 2;
 
 	% Crossbar Bar Dimensions - Keyboard Stand
 
@@ -67,70 +70,77 @@ function [c, ceq] = nlcon_keyboard_strength(x)
 
 	% Input Params - Keyboard Stand
 
-	Wk = 0;
-	Fkx = 0;
-	Fkz = 0;
+	Wk = 0.5 * 9.8; % kg * m / s^2
+	Fkx = 5.2; % N, 25sin12
+	Wsp_k = (Lsp_k * Tsp_k * Hsp_k) * rho;
+	Fkz = 24.45 + Wsp_k; % N, 25cos12
 	W_kz = Wk + Fkz;
-	theta = 0;
+	theta_max = 45;
+    theta_min = 5;
+    
+    theta = theta_max;
+    
 	k = 2; % factor of safety
 
 	% Objective Function Params - Keyboard Stand
 
-	Wcb_k = (pi*Rcb_k^2*Lcb_k);
-	Wst_k = (Lst_k*Tst_k*Hst_k);
-	Wsp_k = (Lsp_k*Tsp_k*Hsp_k);
-	Wl_k = (Tl_k*Ll_k*Hl_k);
-
+	Wcb_k = (pi* Rcb_k^2 * Lcb_k) * rho;
+	Wst_k = (Lst_k * Tst_k * Hst_k) * rho;
+	Wl_k = (Tl_k * Ll_k * Hl_k) * rho;
 
 	% External Reaction Forces - Keyboard Stand
 
-	Ffx = 0.5*(Fkx);
+	Ffx = 0.5 * (Fkx);
 	Fng = 0.5*(W_kz) + 2*Wst_k + 1.5*Wcb_k + Wl_k + 2*Fkx*tan(theta);
 	Fnf = 0.5*(W_kz) + 2*Wst_k + 1.5*Wcb_k + Wl_k - 2*Fkx*tan(theta);
 
 	% Top Crossbar Reaction Forces - Keyboard Stand
 
-	Rcb_az_k = W_kz*(Lsp_k/(Lst_k*cos(theta)));
-	Rcb_bz_k = W_kz*(1 - (Lsp_k/(Lst_k*cos(theta))));
+	Rcb_az_k = W_kz * (Lsp_k / (Lst_k * cos(theta)));
+	Rcb_bz_k = W_kz * (1 - (Lsp_k / (Lst_k * cos(theta))));
 	Rcb_bx_k = -Fkx;
 
 	% Top Crossbar Bending Deflection - Keyboard Stand
 
-	Ist = (Wst_k^3*Tst_k)/12;
-	Icb = (pi*Rcb_k)/4;
+	Ist = (Wst_k^3 * Tst_k) / 12;
+	Icb = (pi * Rcb_k^4) / 4;
 
-	Jcb_az_k = ((Wcb_k*Lcb_k)^3 + 2*Rcb_az_k*((Lcb_k/2) - dsp_k)*(3*Lcb_k^2 - 4*((Lcb_k/2) - dsp_k)^2))/48*Ecb*Icb;
-	Jcb_bz_k = ((Wcb_k*Lcb_k)^3 + 2*Rcb_bz_k*((Lcb_k/2) - dsp_k)*(3*Lcb_k^2 - 4*((Lcb_k/2) - dsp_k)^2))/48*Ecb*Icb;
-	Jcb_bx_k = (2*Rcb_bx_k*((Lcb_k/2) - dsp_k)*(3*Lcb_k^2 - 4*((Lcb_k/2) - dsp_k)^2))/48*Ecb*Icb;
+	Jcb_az_k = ((Wcb_k * Lcb_k^3) + (2 * Rcb_az_k * ((Lcb_k/2) - dsp_k) * ((3 * Lcb_k^2) - (4 * ((Lcb_k/2) - dsp_k)^2))))/ (48 * Ecb * Icb);
+	Jcb_bz_k = ((Wcb_k * Lcb_k^3) + (2 * Rcb_bz_k * ((Lcb_k/2) - dsp_k) * ((3 * Lcb_k^2) - (4 * ((Lcb_k/2) - dsp_k)^2))))/ (48 * Ecb * Icb);
+	Jcb_bx_k = (2 * Rcb_bx_k * ((Lcb_k/2) - dsp_k) * ((3 * Lcb_k^2) - (4 * ((Lcb_k/2) - dsp_k)^2))) / (48 * Ecb * Icb);
 
-	% -sqrt(Jcb_bz_k^2 + Jcb_bx_k^2) < 0.001*m/K ----- whats K?
+    % Top crossbar bending deflection
+	c(1) = sqrt(Jcb_bz_k^2 + Jcb_bx_k^2) - (0.001 / K);
 
 	% Strut Deflection - Keyboard Stand
 
-	Kx = - Rcb_bx_k;
+	Kx = Rcb_bx_k;
 	Kz = Rcb_bz_k;
 	Lz = Rcb_az_k;
 
-	% Jkm = ((Kx*sin(theta) + Kz*cos(theta))*(Lst_k/2)^3)/3*Est*Ist + ((Wst_l/2)*cos(theta)*(Lst_k/4)^2-(3*Lst_k/2) - Lst_k/4)/6*Est*Ist < (0.005*m)/k;
-	% Jkm = ((Kx*cos(theta)*(Lst_k/2)^3)/3*Est*Ist)+((Wst_k/2)*cos(theta)*(Lst_k/4)^2*(3*(Lst_k/2)-Lst_k/4))/6*Est*Ist < (0.005*m)/k;
+	Jkm = (((Kx*sin(theta) + Kz*cos(theta)) * (Lst_k/2)^3) / (3*Est*Ist)) + ((Wst_k/2) * cos(theta) * (Lst_k/4)^2 - ((3 * Lst_k/2) - (Lst_k/4))) / (6*Est*Ist);
+	Jln = ((Lz*cos(theta) * (Lst_k/2)^3) / (3*Est*Ist)) + (((Wst_k/2) * cos(theta) * (Lst_k/4)^2 * ((3*(Lst_k/2)) - Lst_k/4)) / (6*Est*Ist));
+    
+    c(2) = Jkm - (0.005 / K);
+    c(3) = Jln - (0.005 / K);
 
 	% Reaction forces between Xs - Keyboard Stand
 
-	Dz = (0.5*Wk)+Wst_k+(0.75*Wcb_k)+Fkx*tan(theta);
-	Cz = (0.5*W_kz) + Wst_k + (0.75*Wcb_k) + Fkx*tan(theta);
-	Cx = (Cz+Lz)/tan(theta);
+	Dz = (0.5*Wk) + Wst_k + (0.75*Wcb_k) + (Fkx*tan(theta));
+	Cz = (0.5*W_kz) + Wst_k + (0.75*Wcb_k) + (Fkx*tan(theta));
+	Cx = (Cz + Lz) / tan(theta);
 	Dx = -Fkx - Cx;
 
 	% Bottom Strut Reaction Forces - Keyboard Stand
 
-	Az = Wl_k+(0.5*Wcb_k);
+	Az = Wl_k + (0.5*Wcb_k);
 	Bz = Az;
 	Ez = Bz + Cz - Fng;
-	Ex = ((1/tan(theta))*(2*Cz - Ez)) + 2*Cx - Hst_k*Ffx/(Lst_k*sin(theta));
+	Ex = ((1/tan(theta)) * ((2*Cz) - Ez)) + (2*Cx) - (Hst_k * Ffx / (Lst_k * sin(theta)));
 
 	% Shear Failure - Keyboard Stand
 
-	% sqrt(((Ex)^2 + (Ez)^2)/pi*r_b^2) < sigma_b/k ;
+	c(4) = (sqrt(Ex^2 + Ez^2) / pi*r_b^2) - (sigma_b / K);
 
 	% Bottom Crossbar Reaction Forces - Keyboard Stand
 
@@ -138,20 +148,21 @@ function [c, ceq] = nlcon_keyboard_strength(x)
 	Bx = Ax;
 	Qx = Ax;
 
-	Qz = Az - 0.5*Wcb_k;
+	Qz = Az - (0.5 * Wcb_k);
 
 	% Locking Pin Shear Failure - Keyboard Stand
 
 	Ux = Qx;
 	Uz = Qz - Wl_k;
-	% sigma_u = sqrt(Ux^2 + Uz^2) / pi*r_l^2 < sigma_l/k ;
+	sigma_u = sqrt(Ux^2 + Uz^2) / (pi * r_l^2); 
+    c(5) = sigma_u - (sigma_l / k);
 
 	% Bottom Crossbar Bending Deflection - Keyboard Stand
 
-	Uaf_z = ((Wcb_k*Lcb_k)^3 + 2*Qz*((Lcb_k/2 - dl_k)*(3*Lcb_k^2)-4*(Lcb_k/2 - dl_k)^2))/48*Ecb*Icb;
-	Uaf_x = (2*Qx*((Lcb_k/2 - dl_k)*(3*Lcb_k) - 4*(Lcb_k/2 - dl_k)^2))/48*Ecb*Icb;
+	Uaf_z = ((Wcb_k * Lcb_k^3) + (2 * Qz * ((Lcb_k/2) - dl_k) * ((3*Lcb_k^2) - (4*((Lcb_k/2) - dl_k)^2)))) / (48*Ecb*Icb);
+	Uaf_x = (2 * Qx * ((Lcb_k/2) - dl_k) * ((3*Lcb_k^2) - (4 * ((Lcb_k/2) - dl_k)^2))) / (48*Ecb*Icb);
 
-	% sqrt(Uaf_z^2 + Uaf_x^2) < 0.001*m/k;
+	c(6) = sqrt(Uaf_z^2 + Uaf_x^2) - (0.001 / K);
 
 
 
