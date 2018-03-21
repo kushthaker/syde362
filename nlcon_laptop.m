ae5fdf9bcca93443a06f61a88a93f017c691cd81
Tristan Kuehn <tristankuehn@gmail.com>
2018-03-20 23:54:59 -0400

    Adjust keyboard constraints and express others properly
    I have some concerns with some of the laptop equations that I would like to
    address with Brian before I get the whole thing running to produce dimensions.

--- nlcon_laptop.m
+++ nlcon_laptop.m
@@ -1,130 +1,93 @@
-function [c, ceq] = nlcon_laptop_strength(x)
+function [c, ceq] = nlcon_laptop(x)
 
 
 	% keySet = {'rho', 'Rcb_l', 'Lcb_l', 'Lst_l', 'Tst_l', 'Hst_l', 'Lsp_l', 'Tsp_l', 'Hsp_l', 'Tl_l', 'Ll_l', 'Hl_l', 'Rcb_k', 'Lcb_k', 'Lst_k', 'Tst_k', 'Hst_k', 'Lsp_k', 'Tsp_k', 'Hsp_k', 'Tl_k', 'Ll_k', 'Hl_k'};
 
-	% rho = x(1);
-	% Rcb_l = x(2);
-	% Lcb_l = x(3);
-	% Lst_l = x(4);
-	% Tst_l = x(5);
-	% Hst_l = x(6);
-	% Lsp_l = x(7);
-	% Tsp_l = x(8);
-	% Hsp_l = x(9);
-	% Tl_l = x(10);
-	% Ll_l = x(11);
-	% Hl_l = x(12);
-	% Rcb_k = x(13);
-	% Lcb_k = x(14);
-	% Lst_k = x(15);
-	% Tst_k = x(16);
-	% Hst_k = x(17);
-	% Lsp_k = x(18);
-	% Tsp_k = x(19);
-	% Hsp_k = x(20);
-	% Tl_k = x(21);
-	% Ll_k = x(22);
-	% Hl_k = x(23);
-
+    % no nonlinear equalities
+    ceq = [];
+    
 	% Material Properties
-
-	rho = 0;
-	Est = 0;
-	Ecb = 0;
+	rho = 350; % (x(1)) 350 kg / m^3
+	Est = 8500 * 1000; % kPa
+	Ecb = 8500 * 1000; % kPa
 
 	% Bearing and Pin Dimensions
-
-	r_b = 0;
-	sigma_b = 0;
-	r_l = 0;
-	sigma_l = 0;
+    r_b = .0254 / 2 / 2; % half inch diameter
+	sigma_b = 6200; % kPa
+	r_l = .0254 / 2 / 2; % half inch diameter
+	sigma_l = 6200; % kPa
 
 	% Crossbar Dimensions - Laptop Stand
-
-	Rcb_l = 0;
-	Lcb_l = 0;
-
+    Rcb_l = x(2);
+	Lcb_l = x(3);
+    
 	% Strut Dimensions - Laptop Stand
-
-	Tst_l = 0;
-	Hst_l = 0;
-	Lst_l = 0;
+	Lst_l = x(4);
+	Tst_l = x(5);
+	Hst_l = x(6);
 
 	% Support Dimensions - Laptop Stand
-
-	Tsp_l = 0;
-	Hsp_l = 0;
-	Lsp_l = 0;
+	Lsp_l = x(7);
+	Tsp_l = x(8);
+	Hsp_l = x(9);
 	dsp_l = 0;
 
 	% Locking Bar Dimensions - Laptop Stand
-
-	Tl_l = 0;
-	Hl_l = 0;
-	Ll_l = 0;
-	dl_l = 0;
-
+	Tl_l = x(10);
+	Ll_l = x(11);
+	Hl_l = x(12);
+	dl_l = (Lcb_l / 2) - Tst_l;
+    
 	% Objective Function Params - Laptop Stand
-
-	Wcb_l = rho*(pi*Rcb_l^2*Lcb_l);
-    Wst_l = rho*(Lst_l*Tst_l*Hst_l);
-    Wsp_l = rho*(Lsp_l*Tsp_l*Hsp_l);
-    Wl_l = rho*(Tl_l*Ll_l*Hl_l);
-
-
-    Ist = (Wst_k^3 * Tst_k) / 12;
-	Icb = (pi * Rcb_k^4) / 4;
-
-    dmax = 0;
-    dmax_cb = 0;
-    Fna = 0;
-    theta = 0;
     g = 9.81;
+    
+	theta_max = 45;
+    theta_min = 5;
+    
+    theta = theta_max;
 
-    dmax = (dl_l*(3*(Lcb_k)^3 - 4*(dl_l)^2)*sqrt(8^2 + 8^2)) / 24*Ecb*Icb < 1*e - 3
+	Wcb_l = rho * (pi * Rcb_l^2 * Lcb_l) * g;
+    Wst_l = rho * (Lst_l * Tst_l * Hst_l) * g;
+    Wsp_l = rho * (Lsp_l * Tsp_l * Hsp_l) * g;
+    Wl_l = rho * (Tl_l * Ll_l * Hl_l) * g;
+    Fkz = 24.45 + Wsp_l;
 
-    Wl_l*0.25 - Wcb_l + 2*(3*Wst_l+Wcb_l)*(1 - 1/(Lst_l*cos(theta)))
+	Icb = (pi * Rcb_l^4) / 4;
+    
+    % Will this satisfy the overall sum of z forces?
+    Fna = Fkz*0.25 - Wcb_l + 2*(3*Wst_l+Wl_l)*(1 - 1/(Lst_l*cos(theta)));
 
-    % deflection on top bar - laptop stand
+    % Some concerns here - units don't seem to match up, what are x and z?
+    % Also, why aren't there any force components?
+    dmax = (dl_l * (3*(Lcb_k)^3 - 4*(dl_l)^2) * sqrt((8 * x^2) + (8 * z^2))) / (24*Ecb*Icb);
+    
+    c(1) = dmax - 0.001;
 
-    dmax_cb = g*Wcb_l*dl_l*(3*Lcb_l^2 - 4*dl_l^2) / (4*Ecb*Wl_l*Lcb_l^2) < 1*e - 3
+    % deflection on top bar - why the g term? may be confusion over whether
+    % Wcb is a weight or a mass
+    dmax_cb = g*Fkz*dl_l*(3*Lcb_l^2 - 4*dl_l^2) / (4*Ecb*Wl_l*Lcb_l^2);
+    c(2) = dmax_cb - 0.001;
 
-    % beam deflection on strut
-
-    g*Wst_l*(cos(theta) + sin(theta))*Lst_l / (4*Est*Tst_l*Hst_l^3) < 5*e - 3
+    % beam deflection on strut - again, why the g? it doesn't seem to be
+    % present in Brian's notes here
+    dmax_st = g*Fkz*(cos(theta) + sin(theta)) * (Lst_l^3) / (4*Est*Tst_l*Hst_l^3);
+    c(3) = dmax_st - 0.003;
 
     % shear force on s pin
 
-    Sz = Wst_l/2 - Wcb_l - Wst_l + dsp_l*Wst_l/(2*Lst_l)
-    Pz = Wst_l/2
-    Sx = 1*(Sz - Pz)/(tan(theta))
+    Sz = (Fkz/2) - Wcb_l - Wst_l + (dsp_l * Fkz / (2*Lst_l));
+    Pz = Fkz/2;
+    Sx = (Sz/(tan(theta))) - Pz;
 
-    tau_st_l = sqrt(Sx^2 + Sz^2) / (pi*Rcb_l) < sigma_l / 2
+    tau_st_l = sqrt(Sx^2 + Sz^2) / (pi*Rcb_l);
+    c(4) = tau_st_l - (sigma_l / 2);
 
     % Shear force on pin V in lockbar
 
-    Gz = Fna + Wcb_l/2
-    Gx = -Sx
-    Uz = Gz - Wl_l
-    Ux = Gx
+    Gz = Fna + Wcb_l/2;
+    Gx = -Sx;
+    Uz = Gz - Wl_l;
+    Ux = Gx;
 
-    tau_l_l = sqrt(Uz^2 + Ux^2) / pi*r_l^2 < sigma_l / k
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    tau_l_l = sqrt(Uz^2 + Ux^2) / pi*r_l^2;
+    c(5) = tau_l_l - (sigma_l / k);
